3cfb08b079ede4ab2051605ca60244c6e9062927
Apoorve Singhal <sapoorve@gmail.com>
2021-05-13 02:23:47 +0000

    windows script

---
+++ scripts/package-win.sh
@@ -0,0 +1,1 @@
+pyinstaller gui.py --windowed --onefile --add-data 'data/sound.wav;.' --additional-hooks-dir './hooks' --hiddenimport 'cairosvg' --hiddenimport 'cairocffi' -y -n 'Book My Slot' --icon 'data/icons/vaccine.ico'
