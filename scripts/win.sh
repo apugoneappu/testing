a94f4e79b115fe93297f5405135f503e95a17ad6
Apoorve Singhal <sapoorve@gmail.com>
2021-05-16 17:30:30 +0000

    updating windows script

--- scripts/win.sh
+++ scripts/win.sh
@@ -1,1 +1,1 @@
-pyinstaller gui.py --windowed --onefile --add-data 'data/*.wav:data/' --additional-hooks-dir './hooks' --hiddenimport 'cairosvg' --hiddenimport 'cairocffi' -y -n 'Book My Slot' --icon 'data/icons/vaccine.ico'
+pyinstaller gui.py --windowed --onefile --add-data 'data/captcha.wav;data/captcha.wav' --add-data 'data/logout.wav;data/logout.wav' --additional-hooks-dir './hooks' --hiddenimport 'cairosvg' --hiddenimport 'cairocffi' -y -n 'Book My Slot' --icon 'data/icons/vaccine.ico'
